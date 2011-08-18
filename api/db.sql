c484a6d799de4bd2cd81b2ea92a7474d668b9fe2
Jonathan Dell <jldell@gmail.com>
2011-08-18 03:25:37 -0600

    created db design

--- api/db.sql
+++ api/db.sql
@@ -7,7 +7,7 @@ CREATE TABLE `answers` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
 
 
-CREATE TABLE `comment` (
+CREATE TABLE `comments` (
    `id` int(10) not null auto_increment,
    `user_id` int(10) not null,
    `comment` longtext not null,
